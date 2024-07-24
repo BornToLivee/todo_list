19eb8e443eadf5d939b411efab9531d854db1cb3
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-24 12:23:35 +0300

    fix urls by delete unused endpoint

--- task/urls.py
+++ task/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('create/', TaskCreateView.as_view(), name='task-create'),
     path('update/<int:pk>', TaskUpdateView.as_view(), name='task-update'),
     path('delete/<int:pk>', TaskDeleteView.as_view(), name='task-delete'),
-    path('task/<int:pk>/completion/', task_completion, name='task-completion')
+    path('<int:pk>/completion/', task_completion, name='task-completion')
 ]
 
 app_name = "task"
