2f8335330f43626fdf06d78aca6cfa35e678d5ce
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-24 10:27:19 +0300

    add crispy and bootstrap to the project

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "task",
 ]
 
@@ -100,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
