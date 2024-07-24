af242a6e220f9a3285b67de62ea3f0ccabb287f1
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-24 10:59:28 +0300

    create main page task_List

--- task/views.py
+++ task/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "task_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        return context
+
+
+class TaskCreateView(generic.edit.CreateView):
+    model = Task
