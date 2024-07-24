3a716158228ce002589396e4d0ed7719d26c315a
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-07-24 12:48:04 +0300

    create tag create update and delete pages

--- task/urls.py
+++ task/urls.py
@@ -7,7 +7,9 @@ from task.views import (
     TaskDeleteView,
     task_completion,
     TagsListView,
-    TagsCreateView
+    TagsCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 
 urlpatterns = [
@@ -17,7 +19,9 @@ urlpatterns = [
     path('delete/<int:pk>', TaskDeleteView.as_view(), name='task-delete'),
     path('<int:pk>/completion/', task_completion, name='task-completion'),
     path('tags/', TagsListView.as_view(), name='tag-list'),
-    path('tags/create/', TagsCreateView.as_view(), name='tag-create')
+    path('tags/create/', TagsCreateView.as_view(), name='tag-create'),
+    path('tags/update/<int:pk>', TagUpdateView.as_view(), name='tag-update'),
+    path('tags/delete/<int:pk>', TagDeleteView.as_view(), name='tag-delete'),
 ]
 
 app_name = "task"
